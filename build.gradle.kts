9fbb33347f7e9ee8ad28d357d692eb34c5b9267d
hyeonggyu-kim <shokoku0612@gmail.com>
2025-06-14 12:26:01 +0900

    feat: 루트 build.gradle.kts 추가 및 중복 설정 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,3 @@ plugins {
     jacoco
     id("custom.jacoco-aggregate-conventions")
 }
-
-subprojects {
-    repositories {
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
